1e88f4f8c490f65fe7d646ba464b77f4c04c6d21
Aditya Khetarpal <80028073+AdityaKhetarpal@users.noreply.github.com>
2023-08-28 19:19:14 +0530

    Add files via upload

---
+++ CreateAccount.js
@@ -0,0 +1,22 @@
+const algosdk = require('algosdk');
+const fs = require('fs');
+async function createAccountAndExport() {
+  const generatedAccount = algosdk.generateAccount();
+  const passphrase = algosdk.secretKeyToMnemonic(generatedAccount.sk);
+  console.log(`My address: ${generatedAccount.addr}`);
+  console.log(`My passphrase: ${passphrase}`);
+  const dispenser_url =` https://dispenser.testnet.aws.algodev.network/?account=${generatedAccount.addr};`
+  console.log(`Fund the wallet via Algorand Dispenser: ${dispenser_url}`);
+  // Convert the private key to base64 string
+  const privateKeyBase64 = Buffer.from(generatedAccount.sk).toString('base64');
+  // Export the account details as JSON
+  const accountData = {
+    address: generatedAccount.addr,
+    passphrase: passphrase,
+    privateKey: privateKeyBase64,
+  };
+  fs.writeFileSync('account.json', JSON.stringify(accountData, null, 2));
+  console.log("Account details exported to account.json");
+  process.exit();
+}
+createAccountAndExport();
